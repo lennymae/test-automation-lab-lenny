89516172ae0bc0995355d9ee693b9e1f1dc933fe
unknown <maerogero@yahoo.com>
2026-04-14 02:58:38 +0800

    comment out Parabank tcs

--- SeleniumJava_Project/src/test/java/ParaBank/SubmitCustomerInquiryTest.java
+++ SeleniumJava_Project/src/test/java/ParaBank/SubmitCustomerInquiryTest.java
@@ -1,12 +1,12 @@
-package test.java.ParaBank;
-import org.testng.annotations.Test;
-import main.java.Parabank.SubmitCustomerInquiry;
-
-public class SubmitCustomerInquiryTest {
-	 
-	 @Test
-	    public void testUserCreation() {
-		 SubmitCustomerInquiry sci = new SubmitCustomerInquiry();
-		 sci.submitCustomerInquiry();
-	    }
-}
+//package test.java.ParaBank;
+//import org.testng.annotations.Test;
+//import main.java.Parabank.SubmitCustomerInquiry;
+//
+//public class SubmitCustomerInquiryTest {
+//	 
+//	 @Test
+//	    public void testUserCreation() {
+//		 SubmitCustomerInquiry sci = new SubmitCustomerInquiry();
+//		 sci.submitCustomerInquiry();
+//	    }
+//}
--- SeleniumJava_Project/src/test/java/ParaBank/UserCreationTest.java
+++ SeleniumJava_Project/src/test/java/ParaBank/UserCreationTest.java
@@ -1,12 +1,12 @@
-package test.java.ParaBank;
-import org.testng.annotations.Test;
-import main.java.Parabank.UserCreation;
-
-public class UserCreationTest {
-	 
-	 @Test
-	    public void testUserCreation() {
-		 UserCreation uc = new UserCreation();
-		 uc.registerNewUser();
-	    }
-}
+//package test.java.ParaBank;
+//import org.testng.annotations.Test;
+//import main.java.Parabank.UserCreation;
+//
+//public class UserCreationTest {
+//	 
+//	 @Test
+//	    public void testUserCreation() {
+//		 UserCreation uc = new UserCreation();
+//		 uc.registerNewUser();
+//	    }
+//}
